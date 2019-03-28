8412ac260458b3dbc7d3801cd6b1d3be28ce02f5
Jasper van Bourgognie <van.bourgognie@gmail.com>
2019-03-28 01:44:53 +0100

    windows: target .NET 4.5

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly:AssemblyVersion("4.0.1.90")]
-[assembly:AssemblyFileVersion("4.0.1.90")]
+[assembly: AssemblyFileVersion("2019.0328.0056.00")]
 [assembly:AssemblyTitle("Mono.Zeroconf")]
 [assembly:AssemblyDescription("Cross Platform Zeroconf for .NET")]
 [assembly:AssemblyCopyright("Copyright (C) 2006-2009 Novell, Inc.")]
